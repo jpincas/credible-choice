4ef544b545fc68a0c38700d0e4000c068986037c
jpincas <jon@pincas.co.uk>
2019-02-18 09:31:44 +0100

    api testing

---
+++ api-testing/curl.sh
@@ -0,0 +1,8 @@
+## Charities
+curl "http://localhost:5001/appapi/charities"
+
+## Recent Votes
+curl "http://localhost:5001/appapi/recentvotes"
+
+## Vote
+curl -X "POST" "http://localhost:5001/webhooks/vote-fsd8vxcv86vn87s88"
