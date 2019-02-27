7a1ce92eb90c4e82a30cb43c6e2762bc4ee1fdcb
jpincas <jon@pincas.co.uk>
2019-02-27 17:48:54 +0100

    caching

--- server/application.go
+++ server/application.go
@@ -106,14 +106,17 @@ func (a *Application) initRouter(tokenAuth *jwtauth.JWTAuth) {
 	r := chi.NewRouter()
 
 	r.Use(middleware.Recoverer)
+	r.Use(middleware.Throttle(100))
+	r.Use(noCache)
 
 	// Set up routes
 	r.Route("/appapi", func(r chi.Router) {
-		r.Get("/charities", ListCharities)
-		r.Get("/recentvotes", ListRecentVotes)
-		r.Get("/results", GetResults)
+		r.With(cacheFor1Hour).Get("/charities", ListCharities)
+		r.With(cacheFor1Minute).Get("/recentvotes", ListRecentVotes)
+		r.With(cacheFor1Minute).Get("/results", GetResults)
+		r.With(cacheFor1Minute).Get("/representatives", ListRepresentatives)
+
 		r.Post("/prevote", RegisterPreVote)
-		r.Get("/representatives", ListRepresentatives)
 		r.Post("/representatives/search", SearchRepresentative)
 		r.Post("/representatives", CreateRepresentative)
 		r.Delete("/representatives/{id:[A-Z]+}", SuspendRepresentative)
