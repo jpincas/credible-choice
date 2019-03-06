20659b6b10aed22502782969f305f7c9b484359c
allanderek <allan.clark@gmail.com>
2019-03-06 13:44:49 +0000

    Update the notes and remove the old temp09320 hidden alias.

--- alias.sh
+++ alias.sh
@@ -1,5 +1,4 @@
 now alias $1 credible-choice.now.sh
-now alias $1 8399379temp.crediblechoice.uk
 now alias $1 dev.crediblechoice.uk
 now alias $1 www.crediblechoice.uk
 now alias $1 crediblechoice.uk
